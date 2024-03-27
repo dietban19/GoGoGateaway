e5dccc46bed6de45e430bd8aece3ca99294025e4
Uruba <99720011+Sureuba@users.noreply.github.com>
2024-03-27 10:32:27 -0600

    Some testing stuff

--- GoGoGetaway/src/pages/LandingPage/Hero.jsx
+++ GoGoGetaway/src/pages/LandingPage/Hero.jsx
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
-import HeroImage from '../../assets/svgs/hero5.svg';
+import HeroImage from '../../assets/profile/HeroSceneryImage.png';
 import { Typewriter } from 'react-simple-typewriter';
-
 import { useNavigate } from 'react-router-dom';
 import SearchBar from '@/components/SearchBar';
+
 const Hero = () => {
   const navigate = useNavigate(); // Hook to navigate
   const handleType = (count) => {
@@ -15,33 +15,43 @@ const Hero = () => {
   };
 
   return (
-    <div className="relative flex gap-12 bg-light-blue px-24 py-12 pb-48 font-poppins">
-      <div className="flex flex-col items-center justify-center gap-12">
-        <div className="flex h-16 w-full ">
-          {' '}
-          <div className="text-5xl font-bold">
-            Uncover The Secrets of{' '}
-            <Typewriter
-              words={['Calgary', 'Edmonton', 'Banff', 'Canmore']}
-              loop={5}
-              cursor
-              cursorStyle="_"
-              typeSpeed={70}
-              deleteSpeed={50}
-              delaySpeed={1000}
-              onLoopDone={handleDone}
-              onType={handleType}
-            />
+    <div className="relative h-500">
+      <img
+        src={HeroImage}
+        style={{
+          position: 'absolute',
+          top: '138px',
+          left: '350px',
+          width: '1033.5px',
+          height: '802px',
+        }}
+        alt="Hero Image"
+      />
+      <div className="absolute top-0 left-0 p-8">
+        <div className="relative flex flex-col items-start justify-center gap-12 p-20">
+          <div className="flex h-16 w-full">
+            <div className="text-5xl font-bold">
+              Uncover The Secrets of{' '}
+              <Typewriter
+                words={['Calgary', 'Edmonton', 'Banff', 'Canmore']}
+                loop={5}
+                cursor
+                cursorStyle="_"
+                typeSpeed={70}
+                deleteSpeed={50}
+                delaySpeed={1000}
+                onLoopDone={handleDone}
+                onType={handleType}
+              />
+            </div>
+          </div>
+
+          <div className="text-lg font-normal text-secondary-text">
+            Craft your dream trip with personalized recommendations and
+            interactive maps tailored just for you. 
           </div>
         </div>
-
-        <div className="text-lg font-normal text-secondary-text">
-          Craft your dream trip with personalized recommendations and
-          interactive maps tailored just for you. 
-        </div>
       </div>
-      {/* <SearchBar /> */}
-      <img src={HeroImage} className="h-96"></img>
     </div>
   );
 };
---
+++ GoGoGetaway/src/pages/LandingPage/extra.jsx
@@ -0,0 +1,50 @@
+// import React, { useState } from 'react';
+// import HeroImage from '../../assets/profile/HeroSceneryImage.png';
+
+// import { Typewriter } from 'react-simple-typewriter';
+
+// import { useNavigate } from 'react-router-dom';
+// import SearchBar from '@/components/SearchBar';
+// const Hero = () => {
+//   const navigate = useNavigate(); // Hook to navigate
+//   const handleType = (count) => {
+//     // access word count number
+//     // console.log(count);
+//   };
+//   const handleDone = () => {
+//     // console.log(`Done after 5 loops!`);
+//   };
+
+//   return (
+//     <div className="relative flex gap-12 bg-light-blue px-24 py-12 pb-48 font-poppins">
+//       <div className="flex flex-col items-center justify-center gap-12">
+//         <div className="flex h-16 w-full ">
+//           {' '}
+//           <div className="text-5xl font-bold">
+//             Uncover The Secrets of{' '}
+//             <Typewriter
+//               words={['Calgary', 'Edmonton', 'Banff', 'Canmore']}
+//               loop={5}
+//               cursor
+//               cursorStyle="_"
+//               typeSpeed={70}
+//               deleteSpeed={50}
+//               delaySpeed={1000}
+//               onLoopDone={handleDone}
+//               onType={handleType}
+//             />
+//           </div>
+//         </div>
+
+//         <div className="text-lg font-normal text-secondary-text">
+//           Craft your dream trip with personalized recommendations and
+//           interactive maps tailored just for you. 
+//         </div>
+//       </div>
+//       {/* <SearchBar /> */}
+//       <img src={HeroImage} className="h-96"></img>
+//     </div>
+//   );
+// };
+
+// export default Hero;
